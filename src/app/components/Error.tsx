b28d112ec5775022d7e142d37b486a5c5476d370
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-21 20:40:12 +0500

    feat: add Error component

---
+++ src/app/components/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div className='flex h-[100px] w-full items-center justify-center text-[24px] text-[#E00202]'>
+      Something went wrong...
+    </div>
+  );
+};
+
+export default Error;
