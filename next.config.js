483089363276ace562bd878a7a54299bab8ed6a0
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-21 20:37:15 +0500

    fix: add support images from assets.coingecko.com

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+        port: '',
+        pathname: '/coins/images/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
