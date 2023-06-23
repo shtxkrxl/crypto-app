2895b382a57320974dae2fb330cf70c33dac0d93
shtxkrxl <sanya.kalmaev@yandex.ru>
2023-06-23 14:00:20 +0500

    feat: add chart for coins table

---
+++ src/app/components/Chart.tsx
@@ -0,0 +1,60 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Chart = ({ data }: Props) => {
+  const options: ChartOptions<'line'> = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+    elements: {
+      point: {
+        pointStyle: false,
+      },
+    },
+    scales: {
+      x: {
+        display: false,
+      },
+      y: {
+        display: false,
+      },
+    },
+  };
+
+  return <Line data={data} options={options} height={60} width={150} />;
+};
+
+interface Props {
+  data: ChartData<'line'>;
+}
+
+export default Chart;
